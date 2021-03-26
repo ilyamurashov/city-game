87b96808f95223ae94ec3eabaab97cbdf92f9ffb
ilyamurashov <ilyamurashov5677@gmail.com>
2021-03-26 16:51:44 +0300

    добавил файл intervalColors.js с логикой света в окнах

---
+++ js/intervalColors.js
@@ -0,0 +1,72 @@
+setInterval(function winColor(){
+  let wind1 = document.querySelector('.st1');
+  let wind2 = document.querySelector('.st2');
+  let wind3 = document.querySelector('.st3');
+  let wind4 = document.querySelector('.st4');
+  let wind5 = document.querySelector('.st5');
+  let wind6 = document.querySelector('.st6');
+  let wind7 = document.querySelector('.st7');
+  wind1.setAttribute("fill", 'yellow');
+  wind2.setAttribute("fill", 'yellow');
+  wind3.setAttribute("fill", 'yellow');
+  wind4.setAttribute("fill", 'yellow');
+  wind5.setAttribute("fill", 'yellow');
+  wind6.setAttribute("fill", 'yellow');
+  wind7.setAttribute("fill", 'yellow');
+},5000);
+
+setInterval(function winColor1(){
+  let wind1 = document.querySelector('.st1');
+  let wind2 = document.querySelector('.st2');
+  let wind3 = document.querySelector('.st3');
+  let wind4 = document.querySelector('.st4');
+  let wind5 = document.querySelector('.st5');
+  let wind6 = document.querySelector('.st6');
+  let wind7 = document.querySelector('.st7');
+  wind1.setAttribute("fill", ' ');
+  wind2.setAttribute("fill", ' ');
+  wind3.setAttribute("fill", ' ');
+  wind4.setAttribute("fill", ' ');
+  wind5.setAttribute("fill", ' ');
+  wind6.setAttribute("fill", ' ');
+  wind7.setAttribute("fill", ' ');
+}, 15000);
+
+setInterval(function winColor2(){
+  let wind11 = document.querySelector('.st11');
+  let wind12 = document.querySelector('.st12');
+  let wind13 = document.querySelector('.st13');
+  let wind14 = document.querySelector('.st14');
+  let wind15 = document.querySelector('.st15');
+  let wind16 = document.querySelector('.st16');
+  let wind17 = document.querySelector('.st17');
+  let wind18 = document.querySelector('.st18');
+  wind11.setAttribute("fill", 'yellow');
+  wind12.setAttribute("fill", 'yellow');
+  wind13.setAttribute("fill", 'yellow');
+  wind14.setAttribute("fill", 'yellow');
+  wind15.setAttribute("fill", 'yellow');
+  wind16.setAttribute("fill", 'yellow');
+  wind17.setAttribute("fill", 'yellow');
+  wind18.setAttribute("fill", 'yellow');
+},5000);
+
+setInterval(function winColor3(){
+  let wind11 = document.querySelector('.st11');
+  let wind12 = document.querySelector('.st12');
+  let wind13 = document.querySelector('.st13');
+  let wind14 = document.querySelector('.st14');
+  let wind15 = document.querySelector('.st15');
+  let wind16 = document.querySelector('.st16');
+  let wind17 = document.querySelector('.st17');
+  let wind18 = document.querySelector('.st18');
+  wind11.setAttribute("fill", ' ');
+  wind12.setAttribute("fill", ' ');
+  wind13.setAttribute("fill", ' ');
+  wind14.setAttribute("fill", ' ');
+  wind15.setAttribute("fill", ' ');
+  wind16.setAttribute("fill", ' ');
+  wind17.setAttribute("fill", ' ');
+  wind18.setAttribute("fill", ' ');
+}, 20000);
+
